233c79a09c828fb3824792fceb71019a8ff6f5e4
stephane betton <stephane.betton@ag2rlamondiale.fr>
2024-09-09 14:11:25 +0200

    Ajout des dépendances junit-jupiter-params et assertj, configuration de l'encodage UTF-8 et affichage des résultats des tests en français

---
+++ src/main/java/org/jeux/sudoku/Sudoku.java
@@ -0,0 +1,12 @@
+package org.jeux.sudoku;
+
+public class Sudoku {
+    int[][] sudokuGrille;
+    public void setGrille(int[][] sudokuGrilleEmpty) {
+        this.sudokuGrille = sudokuGrilleEmpty;
+    }
+
+    public boolean resoudre() {
+            return false;
+    }
+}
